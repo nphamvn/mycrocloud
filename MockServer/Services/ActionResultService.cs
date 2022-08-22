96564f53f440222b50d1b25897ef2ca1119a50d7
Nam Pham <pvnam95@hotmail.com>
2022-08-22 22:21:49 +0900

    remove unnesscessary

--- MockServer/Services/ActionResultService.cs
+++ MockServer/Services/ActionResultService.cs
@@ -16,20 +16,12 @@ public class ActionResultService : IActionResultService
     }
     public async Task<CustomActionResult> GetActionResult(RequestDto request)
     {
-
-        // SELECT Response.StatusCode
-        // FROM Requests
-        // INNER JOIN User
-        // I
-        // WHERE username = @username AND workspace = @workspace AND requestId = @requestId
-
         var user = await dbContext.Users
                         .Include(u => u.Workspaces.Where(w => w.Name == request.Workspace))
                         .ThenInclude(w => w.Requests.Where(req => req.Method == request.Method && req.Path == request.Path))
                         .ThenInclude(r => r.Response)
                         .Where(u => u.Username == request.Username)
                         .FirstOrDefaultAsync();
-
 
         Response response = null;
         if (user != null)
