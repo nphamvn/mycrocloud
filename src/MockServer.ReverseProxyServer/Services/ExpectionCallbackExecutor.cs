5cfcad9aa14a00d21ebe3af5bfd78529840768fa
nampham <pvnam95@hotmail.com>
2022-11-14 03:46:49 +0900

    add proto file

--- src/MockServer.ReverseProxyServer/Services/ExpectionCallbackExecutor.cs
+++ src/MockServer.ReverseProxyServer/Services/ExpectionCallbackExecutor.cs
@@ -4,17 +4,21 @@ using MockServer.ReverseProxyServer.Models;
 
 namespace MockServer.ReverseProxyServer.Services
 {
-	public class ExpectionCallbackExecutor: IRequestHandler
-	{
-		public ExpectionCallbackExecutor()
-		{
-		}
+    public class ExpectionCallbackExecutor : IRequestHandler
+    {
+        public ExpectionCallbackExecutor()
+        {
+        }
 
         public async Task<AppResponse> Handle(AppRequest request)
         {
-            //1: Get the Class that implements ExpectionCallback interface
+            //1: Prepare source file by replacing user's class file to template source (username, requestId)
 
-            //2: Read and 
+            //2: Send message to Go (or Python) service to build image, start container
+
+            //3: Send request to started container
+
+            return new AppResponse(request.HttpContext, new HttpResponseMessage());
         }
     }
 }
