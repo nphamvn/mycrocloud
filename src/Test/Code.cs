7d94dcdae64b429e0a4957d837b306959b88df85
nampv-bls <nampv@blueline.com.vn>
2022-11-14 10:36:17 +0900

    delete unnesscesary files

--- src/Test/Code.cs
+++
@@ -1,8 +0,0 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTransient < IExpectionCallback, { 0}> ();
-var app = builder.Build();
-app.Run(async (context) =>
-{
-    await new RequestHandler().Hanlde(context);
-});
-app.Run();
