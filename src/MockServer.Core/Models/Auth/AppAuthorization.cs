5ebf2c20e13c4662b72474c2b5485cbceb11c343
nampham <pvnam95@hotmail.com>
2023-01-24 00:21:45 +0900

    edit script

--- src/MockServer.Core/Models/Auth/AppAuthorization.cs
+++ src/MockServer.Core/Models/Auth/AppAuthorization.cs
@@ -2,14 +2,14 @@ namespace MockServer.Core.Models.Auth;
 
 public class AppAuthorization
 {
-    public string DisplayName { get; set; }
+    public string Title { get; set; }
     public string Policy { get; set; }
     public static AppAuthorization Authorize()
     {
-        return new AppAuthorization { DisplayName = nameof(Authorize) };
+        return new AppAuthorization { Title = nameof(Authorize) };
     }
     public static AppAuthorization AllowAnonymous()
     {
-        return new AppAuthorization { DisplayName = nameof(AllowAnonymous) };
+        return new AppAuthorization { Title = nameof(AllowAnonymous) };
     }
 }
