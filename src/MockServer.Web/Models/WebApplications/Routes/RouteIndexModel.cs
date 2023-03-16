d518fd1627b981cd6f4b36fca4baa101578bba5e
Nam Pham <nampv@blueline.com.vn>
2023-03-16 19:37:37 +0900

    sync

--- src/MockServer.Web/Models/WebApplications/Routes/RouteIndexModel.cs
+++ src/MockServer.Web/Models/WebApplications/Routes/RouteIndexModel.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace MockServer.Web.Models.WebApplications.Routes;
 
 public class RouteIndexModel
 {
     public WebApplication WebApplication { get; set; }
     public IEnumerable<RouteIndexItem> Routes { get; set; }
+    public IEnumerable<SelectListItem> HttpMethodSelectListItem { get; set; }
+    public IEnumerable<SelectListItem> AuthorizationTypeSelectListItem { get; set; }
+    public IEnumerable<SelectListItem> IntegrationTypeSelectListItem { get; set; }
 }
