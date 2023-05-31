2f0e20e43a35ae60dc7c31c4fadb5b5a483d41c5
Nam Pham <nampv@blueline.com.vn>
2023-05-31 18:54:23 +0900

    add authenticatio middleware

---
+++ src/MockServer.ApiGateway/Ocelot/Authentication/JwtBearer/ForbiddenContext.cs
@@ -0,0 +1,24 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Ocelot.Authentication.JwtBearer
+{
+    /// <summary>
+    /// A <see cref="ResultContext{TOptions}"/> when access to a resource is forbidden.
+    /// </summary>
+    public class ForbiddenContext : ResultContext<JwtBearerOptions>
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ForbiddenContext"/>.
+        /// </summary>
+        /// <inheritdoc />
+        public ForbiddenContext(
+            HttpContext context,
+            AuthenticationScheme scheme,
+            JwtBearerOptions options)
+            : base(context, scheme, options) { }
+    }
+}
