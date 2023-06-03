85c365383526078b1f51f9d22f981040f7f05df0
Nam Pham <pvnam95@hotmail.com>
2023-06-03 12:01:10 +0900

    add UserName property

--- src/MockServer.Web/Extentions/ClaimsPrincipalExtensions.cs
+++ src/MockServer.Web/Extentions/ClaimsPrincipalExtensions.cs
@@ -10,6 +10,7 @@ namespace MockServer.Web.Extentions
             var user = new IdentityUser
             {
                 Id = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value,
+                UserName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value,
                 Email = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value
             };
             return user;
