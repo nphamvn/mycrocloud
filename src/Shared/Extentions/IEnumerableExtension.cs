d0715d99a37191e45dfbe413a55b35a064426ef1
Nam Pham <pvnam95@hotmail.com>
2024-02-16 22:27:06 +0900

    Remove unused files and update docker-compose.yml

--- src/Shared/Extentions/IEnumerableExtension.cs
+++
@@ -1,9 +0,0 @@
-namespace MycroCloud.Extensions;
-
-public static class IEnumerableExtension
-{
-    public static IEnumerable<T> OrEmptyIfNull<T>(this IEnumerable<T> collection)
-    {
-        return collection ?? Enumerable.Empty<T>();
-    }
-}
