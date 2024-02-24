a6fa05c315f0e17b9fe32d0a0c8af0c259847666
Nam Pham <pvnam95@hotmail.com>
2024-02-24 22:19:31 +0900

    Add unit tests for database migration and model changes check

--- src/Services/WebApp/WebApp.MigrationTest/UnitTest.cs
+++ src/Services/WebApp/WebApp.MigrationTest/UnitTest.cs
@@ -1,24 +1,28 @@
 using Microsoft.EntityFrameworkCore;
+using WebApp.Infrastructure.Repositories.EfCore;
 using WebApp.Migrations;
 
 namespace WebApp.MigrationTest;
 
 public class UnitTest
 {
-    [Fact]
-    public void Should_Not_Has_Pending_Model_Changes()
+    readonly AppDbContext _dbContext;
+    public UnitTest()
     {
         var factory = new AppDbContextFactory();
-        var dbContext = factory.CreateDbContext([]);
-        var result = dbContext.Database.HasPendingModelChanges();
+        _dbContext = factory.CreateDbContext([]);
+    }
+
+    [Fact(DisplayName = "Should Not Has Pending Model Changes")]
+    public void Should_Not_Has_Pending_Model_Changes()
+    {
+        var result = _dbContext.Database.HasPendingModelChanges();
         Assert.False(result);
     }
 
-    [Fact]
+    [Fact(DisplayName = "Should Migrate Database")]
     public void Should_Migrate_Database()
     {
-        var factory = new AppDbContextFactory();
-        var dbContext = factory.CreateDbContext([]);
-        dbContext.Database.Migrate();
+        _dbContext.Database.Migrate();
     }
 }
