4624b2aa15a819fff2ba8b1f65040509cff44780
Nam Pham <pvnam95@hotmail.com>
2024-03-01 20:41:12 +0900

    Rename LogRemoteIp to RemoteAddress

---
+++ src/Services/WebApp/WebApp.Migrations/Migrations/20240301114059_RenameLogRemoteIpToRemoteAddress.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameLogRemoteIpToRemoteAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RemoteIp",
+                table: "Logs",
+                newName: "RemoteAddress");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RemoteAddress",
+                table: "Logs",
+                newName: "RemoteIp");
+        }
+    }
+}
