67a06473412edeb0cd540b4f7508923e9602ea0a
Nam Pham <pvnam95@hotmail.com>
2024-03-06 15:10:05 +0900

    IntelliSense for TypeScript

--- src/WebApp/src/vite-env.d.ts
+++ src/WebApp/src/vite-env.d.ts
@@ -1,2 +1,15 @@
 /// <reference types="vite/client" />
 declare const __COMMIT_HASH__: string;
+
+//https://vitejs.dev/guide/env-and-mode#intellisense-for-typescript
+interface ImportMetaEnv {
+    readonly VITE_BASE_API_URL: string;
+    readonly VITE_AUTH0_DOMAIN: string;
+    readonly VITE_AUTH0_CLIENTID: string;
+    readonly VITE_AUTH0_AUDIENCE: string;
+    readonly VITE_WEBAPP_APIGATEWAY_DOMAIN: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
