9ddd25eb8f8b438b2a1e7bb062b101d65f40c04f
Nam Pham <pvnam95@hotmail.com>
2024-03-18 23:39:04 +0900

    Add FileFolderItem class to FilesController.cs

--- src/Services/WebApp/WebApp.RestApi/Controllers/FilesController.cs
+++ src/Services/WebApp/WebApp.RestApi/Controllers/FilesController.cs
@@ -35,6 +35,12 @@ public class FilesController(AppDbContext appDbContext) : BaseController
     }
 }
 
+public class FileFolderItem {
+    public string Type { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
 public class CreateUpdateFolderModel
 {
     public string? Name { get; set; }
