f614fb4807073aa0bb460b0a92570dffe593c024
Nam Pham <pvnam95@hotmail.com>
2024-05-16 14:55:47 +0900

    apply route enabled to api gateway

--- src/Services/WebApp/WebApp.MiniApiGateway/RouteResolverMiddleware.cs
+++ src/Services/WebApp/WebApp.MiniApiGateway/RouteResolverMiddleware.cs
@@ -1,52 +1,56 @@
 ﻿using Microsoft.AspNetCore.Routing.Template;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Domain.Entities;
 using WebApp.Domain.Enums;
 using WebApp.Domain.Repositories;
+using WebApp.Infrastructure.Repositories.EfCore;
 using Route = WebApp.Domain.Entities.Route;
 
 namespace WebApp.MiniApiGateway;
 
 public class RouteResolverMiddleware(RequestDelegate next)
 {
-    public async Task Invoke(HttpContext context, IRouteRepository routeRepository, ILogRepository logRepository)
+    public async Task Invoke(HttpContext context, ILogRepository logRepository,
+        AppDbContext appDbContext)
     {
-            var app = (App)context.Items["_App"]!;
-            var routes = await routeRepository.List(app.Id, "", "");
-            var matchedRoutes = new List<Route>();
-            foreach (var r in routes)
+        var app = (App)context.Items["_App"]!;
+        var routes = await appDbContext.Routes.Where(r => r.App == app && r.Enabled).ToListAsync();
+        var matchedRoutes = new List<Route>();
+        foreach (var r in routes)
+        {
+            var matcher = new TemplateMatcher(TemplateParser.Parse(r.Path), []);
+            if (matcher.TryMatch(context.Request.Path, context.Request.RouteValues) &&
+                (r.Method.Equals("ANY") || context.Request.Method.Equals(r.Method, StringComparison.OrdinalIgnoreCase)))
             {
-                var matcher = new TemplateMatcher(TemplateParser.Parse(r.Path), []);
-                if (matcher.TryMatch(context.Request.Path, context.Request.RouteValues) && 
-                    (r.Method.Equals("ANY") || context.Request.Method.Equals(r.Method, StringComparison.OrdinalIgnoreCase)))
-                {
-                    matchedRoutes.Add(r);
-                }
+                matchedRoutes.Add(r);
             }
-        
-            switch (matchedRoutes.Count)
-            {
-                case 0:
-                    context.Response.StatusCode = 404;
-                    return;
-                case > 1:
-                    context.Response.StatusCode = 500;
-                    await context.Response.WriteAsync("The request matched multiple endpoints");
-                    return;
-            }
-        
-            var route = matchedRoutes.First();
-            switch (route.Status)
-            {
-                case RouteStatus.Inactive:
-                    context.Response.StatusCode = 404;
-                    return;
-                case RouteStatus.Blocked:
-                    context.Response.StatusCode = 403;
-                    await context.Response.WriteAsync("The request matched a blocked endpoint");
-                    return;
-            }
-            context.Items["_Route"] = route;
-            await next(context);
+        }
+
+        switch (matchedRoutes.Count)
+        {
+            case 0:
+                context.Response.StatusCode = 404;
+                return;
+            case > 1:
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("The request matched multiple endpoints");
+                return;
+        }
+
+        var route = matchedRoutes.First();
+        switch (route.Status)
+        {
+            case RouteStatus.Inactive:
+                context.Response.StatusCode = 404;
+                return;
+            case RouteStatus.Blocked:
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("The request matched a blocked endpoint");
+                return;
+        }
+
+        context.Items["_Route"] = route;
+        await next(context);
     }
 }
 
