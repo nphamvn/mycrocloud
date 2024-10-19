dd4dc2e8eb0d279ec3b1bd172e0767516e591a94
Nam Pham <pvnam95@hotmail.com>
2024-10-19 17:51:47 +0900

    Refactor List component to add delete functionality for text storages

--- src/WebApp/src/pages/storages/TextStorages/List.tsx
+++ src/WebApp/src/pages/storages/TextStorages/List.tsx
@@ -3,11 +3,14 @@ import ITextStorage from "./ITextStorage";
 import { useAuth0 } from "@auth0/auth0-react";
 import { AppContext } from "../../apps";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function List() {
   const app = useContext(AppContext)!;
   const { getAccessTokenSilently } = useAuth0();
+
   const [storages, setStorages] = useState<ITextStorage[]>([]);
+
   useEffect(() => {
     const getStorages = async () => {
       const accessToken = await getAccessTokenSilently();
@@ -21,6 +24,32 @@ export default function List() {
     };
     getStorages();
   }, []);
+
+  const handleDeleteClick = async (storage: ITextStorage) => {
+    if (
+      !window.confirm(
+        `Are you sure you want to delete the storage "${storage.name}"?`,
+      )
+    ) {
+      return;
+    }
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(
+      `/api/apps/${app.id}/textstorages/${storage.id}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    if (response.ok) {
+      toast.success(`Storage "${storage.name}" has been deleted.`);
+      setStorages(storages.filter((s) => s.id !== storage.id));
+    }
+  };
+
   return (
     <div className="p-2">
       <h1 className="font-bold">Text Storages</h1>
@@ -28,7 +57,7 @@ export default function List() {
         <Link to={"new"} className="bg-primary px-3 py-1 text-white">
           Create Storage
         </Link>
-        <table className="mt-2 w-full">
+        <table className="mt-2 w-full p-2">
           <thead>
             <tr>
               <th className="text-start">Name</th>
@@ -41,19 +70,26 @@ export default function List() {
           <tbody>
             {storages.map((s) => (
               <tr key={s.id}>
-                <td>{s.name}</td>
+                <td>
+                  <Link to={`${s.id}`} className="text-blue-500">
+                    {s.name}
+                  </Link>
+                </td>
                 <td>{s.size}</td>
                 <td>{new Date(s.createdAt).toLocaleString()}</td>
                 <td>
                   {s.updatedAt ? new Date(s.updatedAt).toLocaleString() : "-"}
                 </td>
                 <td className="flex space-x-2 divide-x">
-                  <Link to={`${s.id}`} className=" text-blue-500">
-                    Edit
-                  </Link>
                   <Link to={`${s.id}/logon`} className="text-blue-500">
                     Logon
                   </Link>
+                  <button
+                    onClick={() => handleDeleteClick(s)}
+                    className="text-red-500"
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
