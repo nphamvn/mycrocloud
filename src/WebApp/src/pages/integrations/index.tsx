b1a93da6e994ee4c4746d62cf82041605ca541c1
Nam Pham <pvnam95@hotmail.com>
2024-10-25 00:07:45 +0900

    Refactor react-router-dom import in integrations/index.tsx

--- src/WebApp/src/pages/integrations/index.tsx
+++ src/WebApp/src/pages/integrations/index.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AppContext } from "../apps";
 
 export interface GitHubRepo {
